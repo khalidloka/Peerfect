373a8344d3afde8856de1a349740f57cabb1fb61
khalidloka <96070737+khalidloka@users.noreply.github.com>
2024-02-05 17:05:05 +0100

    adding pages

---
+++ frontend/app/JeSuisProf/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>je suis prof
+        
+    </div>
+  )
+}
--- frontend/components/Home/JeSuisUnCentre.tsx
+++ frontend/components/Home/JeSuisUnCentre.tsx
@@ -2,40 +2,50 @@
 import React, { useState } from 'react'
 
 import profcentre from '@/public/profCenter.svg'
-import profGreen from '@/public/profGeen.svg'
-import centre from '@/public/centre.svg'
+import profgreen from '@/public/profgreen.svg'
 
 import Image from 'next/image'
 import Link from 'next/link'
 
+
 export default function JeSuisUnCentre() {
-  
-  const [BgChanger, setBgChanger]=useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-  
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-
     <div className='relative -mt-8'>
-
-      
-<div className="absolute top-96 hover:bg-lime-400 left-96 w-[273.96px] h-[71.05px] bg-white rounded-[17px] shadow text-center pt-6 text-blue-800 text-xl font-bold ">
-        <Link href='/Professeur' >JE SUIS UN PROFESSEUR</Link>
+      <div
+        className={`absolute top-96 left-96 w-[273.96px] h-[71.05px] bg-white rounded-[17px] shadow text-center pt-6 text-blue-800 text-xl font-bold hover:bg-lime-400`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <Link href='/JeSuisProf'>JE SUIS UN PROFESSEUR</Link>
       </div>
-     <div className="relative top-96 hover:bg-lime-400 left-2/3 w-[273.96px] h-[71.05px] bg-white rounded-[17px] shadow text-center pt-6 text-blue-800 text-xl font-bold ">
-        <Link href='/Centre'>JE SUIS UN CENTRE</Link>
+      <div className="relative top-96 left-2/3 w-[273.96px] h-[71.05px] bg-white rounded-[17px] shadow text-center pt-6 text-blue-800 text-xl font-bold hover:bg-lime-400">
+        <Link href='/JeSuisCentre'>JE SUIS UN CENTRE</Link>
       </div>
-       <Image
-        className=""
-        src={profcentre}
-        alt=""
-        style={{
-          width: '100%',
-         
-        }}
-        priority
-      />
+    {isHovered? <Image
+  className=""
+  src={ profgreen}
+  alt=""
+  style={{
+    width: '100vw',
 
-      
+  }}
+  priority
+/>:<Image
+  className="w-full"
+  src={profcentre}
+  alt=""
+  priority 
+/>} 
     </div>
   )
 }
