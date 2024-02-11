03a5998f83cf84781711c751a1fb9ee8c2f28515
mohamed ettarni <57720814+ETTARNI-med@users.noreply.github.com>
2024-02-11 18:49:01 +0100

    adjust the image in the second section of the about page

--- frontend/app/APropos/page.tsx
+++ frontend/app/APropos/page.tsx
@@ -10,7 +10,6 @@ import Image6 from "../../public/image6.png";
 export default function page() {
   return (
     <div className="bg-Apropo pt-12">
-
       {/* first section */}
 
       <div className="flex gap-x-8  ">
@@ -47,48 +46,48 @@ export default function page() {
       </div>
       {/* second section */}
       <div className="flex gap-x-8 mt-20">
-      <div className="flex w-48  basis-1/2">
-          <Image
-            src={Image4}
-            alt="Photo de la fondatrice"
-            className="pt-12"
-          ></Image>
-          <Image
-            src={Image5}
-            alt="Photo de la fondatrice"
-            className="pb-12"
-          ></Image>
-         
+        <div className="flex w-48  basis-1/2">
+          <div className=" rounded-xl">
+            {" "}
+            <Image
+              src={Image4}
+              alt="Photo de la fondatrice"
+              className="pt-12 rounded-xl"
+            ></Image>
+          </div>
+          <div className="">
+            <Image
+              src={Image5}
+              alt="Photo de la fondatrice"
+              className="pb-12"
+            ></Image>
+          </div>
         </div>
 
         <div className="flex flex-col justify-center basis-1/2 p-16">
-          <h1 className="text-4xl text-blue-800 font-bold	">
-            Notre mission {" "}
-          </h1>
+          <h1 className="text-4xl text-blue-800 font-bold	">Notre mission </h1>
           <p className="pt-4 text-lg">
-          Favoriser la synergie humaine via un apprentissage collaboratif de qualité et développer les compétences techniques et professionnelles des apprenants, en les préparant à des carrières spécifiques et en favorisant l'acquisition de compétences recherchées sur le marché du travail.
+            Favoriser la synergie humaine via un apprentissage collaboratif de
+            qualité et développer les compétences techniques et professionnelles
+            des apprenants, en les préparant à des carrières spécifiques et en
+            favorisant l'acquisition de compétences recherchées sur le marché du
+            travail.
           </p>
         </div>
-        
       </div>
       {/* Third section section */}
       <div className="flex gap-x-8 mt-20">
         <div className="flex flex-col justify-center basis-1/2 p-16">
           <h1 className="text-4xl text-blue-800 font-bold	pb-12">
-          Notre innovation pédaghogique{" "}
+            Notre innovation pédaghogique{" "}
           </h1>
           <p className="pt-4 text-xl">
-          Adopter des méthodes d'enseignement novatrices et des technologies éducatives pour améliorer l'expérience d'apprentissage
+            Adopter des méthodes d'enseignement novatrices et des technologies
+            éducatives pour améliorer l'expérience d'apprentissage
           </p>
         </div>
         <div className=" basis-1/2 rounded-s-3xl rounded-r-3xl bg-blue-800 pl-12 mr-12 ">
-
-          <Image
-            src={Image6}
-            alt="Photo de la fondatrice"
-            className=""
-          ></Image>
-        
+          <Image src={Image6} alt="Photo de la fondatrice" className=""></Image>
         </div>
       </div>
     </div>
